f839016204e8e6429abe149832e5107e557784ce
Hpupq <puppala.harish1@gmail.com>
2018-09-01 23:11:36 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,12 +1,7 @@
 var express= require('express');
 var app=express();
-app.get('/',function(request,response){
-    response.send("World HI page");
-});
-app.get('/books',function(request,response){
-    response.send("Books Page ");
-});
-app.get('/hostel',function(req,res){
+
+app.get('/',function(req,res){
 res.sendFile(__dirname+'/Index.html')
 });
 var port=process.env.PORT || 8080
